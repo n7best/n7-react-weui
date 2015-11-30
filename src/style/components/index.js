9d6ca35607a7f505cdf039e2975351c654bc4aca
n7best <root@Charlies-MacBook-Pro-2.local>
2015-11-30 17:15:04 -0500

    sync patchs with weui style

---
+++ src/style/components/index.js
@@ -0,0 +1,26 @@
+import Cells from './cells';
+import CellBody from './cells/cell-body';
+import CellFooter from './cells/cell-footer';
+import CellHeader from './cells/cell-header';
+import Cell from './cells/cell';
+import CellsTips from './cells/cells-tips';
+import CellsTitle from './cells/cells-title';
+import CheckboxCell from './cells/checkbox-cell';
+import InputCell from './cells/input-cell';
+import Select from './cells/input-select';
+import RadioCell from './cells/radio-cell';
+import RadioCells from './cells/radio-cells';
+import SwitchCell from './cells/switch-cell';
+import Button from './button';
+import ButtonArea from './button';
+import Actionsheet from './actionsheet';
+import Article from './article';
+import Dialog from './dialog';
+import Mask from './mask';
+import Message from './message';
+import Progress from './progress';
+import Toast from './toast';
+
+
+export default {Toast, Progress, Message, Mask, Dialog, Cells, CellBody, CellFooter, CellHeader, Cell, CellsTips, CellsTitle, CheckboxCell, InputCell, Button, Select, RadioCell, RadioCells, SwitchCell, ButtonArea, Actionsheet, Article}
+
--- src/style/index.js
+++
@@ -1,5 +0,0 @@
-import Cells from './components/cells';
-import Button from './components/button';
-
-export default {Cells, Button}
-
